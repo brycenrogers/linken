9b9ece71e978bbb7838c0d0f7f3208dbfbe25958
Brycen Rogers <brycen@Brycens-MacBook-Pro.local>
2015-09-25 13:43:28 -0600

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! Linken <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: [
+                    'resources/assets/js/libs/*.js',
+                    'resources/assets/js/*.js'
+                ],
+                dest: 'resources/assets/js/linken.min.js'
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['assets/dev/sass/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                    interval: 3007
+                },
+            },
+        },
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'assets/dist/css/base.css': 'assets/dev/sass/base.scss'
+                }
+            }
+        },
+        processhtml: {
+            options: {},
+            dist: {
+                files: {
+                    'dist/index.html': ['index.html']
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'assets/dist/css/linken.min.css': ['assets/dist/css/*.css']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('dist', ['uglify', 'sass', 'cssmin', 'processhtml']);
+
+};
---
+++ resources/assets/js/settings.js
@@ -0,0 +1,14 @@
+//  ====================  Settings JS  ====================
+
+//    created by:        Brycen Rogers ( brycenrogers@gmail.com )
+//    creation data:     September 25, 2015
+
+//    This file contains javascript for the base Add pane of Linken
+
+//  ===========================================================
+
+$( document ).ready(function() {
+
+    
+
+));
